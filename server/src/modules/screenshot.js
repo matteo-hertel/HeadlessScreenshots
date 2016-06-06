a03ebd7cd657cb8268ea95f111b351287cfba1cd
Matteo Hertel <info@matteohertel.com>
2016-06-06 21:54:22 +0100

    [BREAKING CHANGES] moving stuff around to build the final docker server

--- server/src/modules/screenshot.js
+++ server/src/modules/screenshot.js
@@ -24,18 +24,21 @@ exports.screenshot = function(url, output, dimensions) {
         }
         if (err) {
             eventEmitter.emit("error", {
+                key: dimensions.replace(/\*/g, ""),
                 status: "error",
                 message: err
             });
         }
         if (stdout) {
             eventEmitter.emit("success", {
+                key: dimensions.replace(/\*/g, ""),
                 status: "success",
                 message: stdout
             });
         }
         if (stderr) {
             eventEmitter.emit("stderror", {
+                key: dimensions.replace(/\*/g, ""),
                 status: "stderror",
                 message: stderr
             });
